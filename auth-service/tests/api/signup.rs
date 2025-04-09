188a501bce03264159a8f7cff5e08ee17433e2db
Dan Dresser <ddresser05@gmail.com>
2025-04-09 07:13:31 -0500

    [Sprint 1][Task 7] feat: implemented signup route interface

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,34 @@
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn signup_succeeds() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = TestApp::get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true,
+        }),
+        serde_json::json!({
+            "email": &random_email,
+            "requires2FA": true,
+        }),
+        serde_json::json!({
+            "email": &random_email,
+            "password": "password123",
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failedd for input: {:?}",
+            test_case,
+        );
+    }
 }
