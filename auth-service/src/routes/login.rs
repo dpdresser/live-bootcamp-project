265254f5dd1e42c270ac50907438b1b1d259515a
Dan Dresser <ddresser05@gmail.com>
2025-04-23 12:15:25 -0500

    [Sprint 3][Task 1] feat: implemented error cases for login route

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,38 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{app_state::AppState, domain::{AuthAPIError, Email, Password}};
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = 
+        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    
+    let password = 
+        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    let user_store = state.user_store.read().await;
+
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return Err(AuthAPIError::IncorrectCredentials);
+    }
+
+    let _user = user_store.get_user(&email).await.map_err(|_| AuthAPIError::IncorrectCredentials)?;
+
+    Ok(StatusCode::OK)
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct LoginResponse {
+    pub message: String,
+    #[serde(rename = "loginAttemptId")]
+    pub login_attempt_id: String,
 }
