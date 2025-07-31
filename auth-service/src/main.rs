c2949e1f8fecabaafffa40b56720b36f06f03cf1
Dan Dresser <ddresser05@gmail.com>
2025-07-30 21:36:42 -0500

    [Misc] fix: testing new code

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -5,6 +5,7 @@ use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
+    println!("🚀 Auth Service starting up! Version: 2024-07-31");
     let user_store = Arc::new(RwLock::new(HashMapUserStore::default()));
     let app_state = AppState::new(user_store);
 
@@ -13,5 +14,4 @@ async fn main() {
         .expect("Failed to build application");
 
     app.run().await.expect("Failed to run application");
-    println!("🚀 Auth Service starting up! Version: 2024-07-31");
 }
