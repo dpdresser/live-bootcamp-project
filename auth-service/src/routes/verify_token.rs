397adf3b5927c06ecf69d019d3900269c4c1b291
Dan Dresser <ddresser05@gmail.com>
2025-08-04 16:59:53 -0500

    [Sprint 3][Task 4] feat: implemented verify_token route

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,18 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{domain::AuthAPIError, utils::validate_token};
+
+pub async fn verify_token(
+    Json(request): Json<VerifyTokenRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    validate_token(&request.token)
+        .await
+        .map_err(|_| AuthAPIError::InvalidToken)?;
+    Ok(StatusCode::OK.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    token: String,
 }
